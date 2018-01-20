b797d4099006f0af72946eb677d6ecda142c9666
Tolga Yapici -- MSU Postdoc <tyapici@aspen08.private.pa.umd.edu>
2018-01-20 16:25:57 -0500

    analysis script is changed a bit to address matplotlib use at clusters

--- Limit_calculations/py/VirgoCluster_annihilation_extended.py
+++ Limit_calculations/py/VirgoCluster_annihilation_extended.py
@@ -1,3 +1,6 @@
+# set matplotlib
+import matplotlib as mpl
+mpl.use("agg")
 
 # stop ROOT hijacking the command line options
 import ROOT
@@ -35,7 +38,7 @@ class Sources():
 
     def set_model(self, model_name):
         self.model_name = model_name
-        
+
     def set_M87(self):
         print(DM_model)
         # extended template
@@ -57,7 +60,7 @@ class Sources():
         spec_M87.J.fix         = True
         # set source
         self.source_M87 = ExtendedSource("M87",spatial_shape=shape_M87,spectral_shape=spec_M87)
-    
+
     def set_M49(self):
         # extended template
         shape_M49    = SpatialTemplate_2D()
@@ -78,7 +81,7 @@ class Sources():
         spec_M49.J.fix         = True
         # set source
         self.source_M49 = ExtendedSource("M49",spatial_shape=shape_M49,spectral_shape=spec_M49)
-    
+
     def set_M87_pointsource(self, data):
         if data=='HESS-lower':
             index = -2.62
@@ -102,7 +105,7 @@ class Sources():
         spec_M87_point.K.fix = True
         # set source
         self.point_M87  = PointSource("M87Point", ra=187.70, dec=12.39, spectral_shape=spec_M87_point)
-    
+
     def set_ROI(self):
         self.ROI_radius = np.max([self.ra_M87-self.ra_M49, self.dec_M87-self.dec_M49])+10.
         self.ROI_RA     = np.mean([self.ra_M87, self.ra_M49])
@@ -113,9 +116,9 @@ class Sources():
         self.set_M49()
         self.set_M87_pointsource(data)
         self.set_ROI()
-        
+
 class Identity(Function1D):
-    r"""                                                                                                                              
+    r"""
     description :
         Identity function
 
@@ -159,7 +162,7 @@ if add_point_source:
         experiment = parsed_args.exp#sys.argv[5]
     except:
         experiment = 'VERITAS'
-        
+
 verbose = parsed_args.verbose#True
 print("running for mass {} GeV".format(mass))
 print("            channel {}".format(channel))
@@ -175,7 +178,7 @@ else:
     model = Model(sources.source_M87, sources.source_M49,
                   sources.point_M87)
 
-model.link(model.M87.spectrum.main.DMAnnihilationFlux.sigmav, 
+model.link(model.M87.spectrum.main.DMAnnihilationFlux.sigmav,
            model.M49.spectrum.main.DMAnnihilationFlux.sigmav,
            Identity())
 
@@ -198,7 +201,7 @@ if calc_TS:
     print("TS_max: {}".format(best_fit_TS))
     print("Best fit sigmav: {}".format(model.M87.spectrum.main.DMAnnihilationFlux.sigmav))
     llh.write_model_map("../results/model_maps/{}GeV_{}.root".format(mass, channel))
-    
+
 # calculate the profile for 95% CL computation
 search_size = 40
 best_fit    = model.M87.spectrum.main.DMAnnihilationFlux.sigmav.value
@@ -222,7 +225,6 @@ imin   = np.argmin(delLLs)
 plt.semilogx(10**norms, delLLs)
 plt.xlim(np.log10(best_fit)-.1, np.log10(best_fit)+.6)
 plt.ylim(0, 3)
-
 plt.savefig("../results/LL_profiles/annihilation_{}GeV_{}_{}.eps".format(mass, channel, DM_model))
 interpolator = interp1d(delLLs[imin:],10**norms[imin:], kind='cubic', fill_value='extrapolate')
 norm_95cl = interpolator(2.71/2.)
